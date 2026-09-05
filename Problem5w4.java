20908ea44970939f45ba4c5c7ae9d99bac0fd90e
Priyanka <as4128@srmist.edu.in>
2026-09-05 19:29:28 +0530

    Add Problem5w4 find minimum

---
+++ Problem5w4.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class Problem5w4 {
+
+    static int findMin(int[] nums) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return nums[left];
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {3, 4, 5, 1, 2};
+
+        int result = findMin(nums);
+
+        System.out.println("Input: " + Arrays.toString(nums));
+        System.out.println("Minimum element = " + result);
+    }
+}
