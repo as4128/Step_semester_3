0a35c3ee41988253650c5169ca1c3e89ade6fbff
Priyanka <as4128@srmist.edu.in>
2026-09-05 19:43:26 +0530

    Add Problem4w4p4 merge sorted array

---
+++ Problem4w4p4.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+
+public class Problem4w4p4 {
+
+    static int[] mergeSortedArrays(int[] arr1, int[] arr2) {
+
+        int[] result = new int[arr1.length + arr2.length];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < arr1.length && j < arr2.length) {
+
+            if (arr1[i] < arr2[j]) {
+                result[k] = arr1[i];
+                i++;
+            } else {
+                result[k] = arr2[j];
+                j++;
+            }
+
+            k++;
+        }
+
+        // Copy remaining elements of arr1
+        while (i < arr1.length) {
+            result[k] = arr1[i];
+            i++;
+            k++;
+        }
+
+        // Copy remaining elements of arr2
+        while (j < arr2.length) {
+            result[k] = arr2[j];
+            j++;
+            k++;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr1 = {1, 3, 5};
+        int[] arr2 = {2, 4, 6};
+
+        int[] result = mergeSortedArrays(arr1, arr2);
+
+        System.out.println("Array 1: " + Arrays.toString(arr1));
+        System.out.println("Array 2: " + Arrays.toString(arr2));
+        System.out.println("Merged Array: " + Arrays.toString(result));
+    }
+}
